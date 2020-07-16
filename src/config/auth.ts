994243818c1512e9331682f8dbf8942fefcf4ee5
luiz Spinelli <luiz.lucchi@gmail.com>
2020-07-16 15:50:35 -0300

    initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'a634a3e8f12063899ea986471044051b',
+        expiresIn: '1d',
+    },
+};
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import './database/index';
+import 'reflect-metadata';
+import cors from 'cors';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+
+// src/server.ts
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
+
+app.listen(3333, () => {
+    console.log('Server started on port 3333');
+});
