bc8b725995903dc9d4695d35d7d598f6a1569632
Luiz Spinelli <luiz.lucchi@gmail.com>
2020-07-23 20:27:01 -0300

    createAppointService test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
@@ -19,7 +20,25 @@ describe('CreateAppointment', () => {
         expect(appointment.provider_id).toBe('123123123');
     });
 
-    // it('should not be able to create a new appointment at the same time', () => {
-    //     expect(1 + 2).toBe(3);
-    // });
+    it('should not be able to create a new appointment at the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 6, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123123123',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '123123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
