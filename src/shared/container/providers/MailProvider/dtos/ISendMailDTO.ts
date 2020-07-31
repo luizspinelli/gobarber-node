a20887db9a7338e8fa2d11982973ed7a79b648a6
Luiz Spinelli <luiz.lucchi@gmail.com>
2020-07-31 17:13:08 -0300

    refactoring providers containers

--- src/shared/container/providers/MailProvider/dtos/ISendMailDTO.ts
+++ src/shared/container/providers/MailProvider/dtos/ISendMailDTO.ts
@@ -1,13 +1,13 @@
 import IParseMailTemplateDTO from '../../MailTemplateProvider/dtos/IParseMailTemplateDTO';
 
 interface IMailContact {
-    name: string;
-    mail: string;
+  name: string;
+  mail: string;
 }
 
 export default interface ISendMailDTO {
-    to: IMailContact;
-    from?: IMailContact;
-    subject: string;
-    templateData: IParseMailTemplateDTO;
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplateDTO;
 }
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,29 +1,5 @@
-import { container } from 'tsyringe';
+import './MailTemplateProvider';
 
-import mailConfig from '@config/mail';
+import './StorageProvider';
 
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implemantations/DiskStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementation/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementation/SESMailProvider';
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementation/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import './MailProvider';
