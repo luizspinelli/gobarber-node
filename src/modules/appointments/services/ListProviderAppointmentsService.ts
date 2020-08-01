2debfd2562fe52f5dfa9734b27704e00460654c6
Luiz Spinelli <luiz.lucchi@gmail.com>
2020-08-01 20:28:49 -0300

    redis cache provider config

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,40 +1,49 @@
 import { inject, injectable } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 interface Request {
-    provider_id: string;
-    month: number;
-    year: number;
-    day: number;
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
 }
 
 @injectable()
 class ListProviderAppointmentsService {
-    constructor(
-        @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository,
-    ) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
 
-    public async execute({
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: Request): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findaAllInDayFromProvider(
+      {
         provider_id,
+        day,
         month,
         year,
-        day,
-    }: Request): Promise<Appointment[]> {
-        const appointments = await this.appointmentsRepository.findaAllInDayFromProvider(
-            {
-                provider_id,
-                day,
-                month,
-                year,
-            },
-        );
+      },
+    );
 
-        return appointments;
-    }
+    await this.cacheProvider.save('asd', 'cache');
+
+    const dataCache = await this.cacheProvider.recover('asd');
+
+    console.log(dataCache);
+
+    return appointments;
+  }
 }
 
 export default ListProviderAppointmentsService;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,3 +3,5 @@ import './MailTemplateProvider';
 import './StorageProvider';
 
 import './MailProvider';
+
+import './CacheProvider';
