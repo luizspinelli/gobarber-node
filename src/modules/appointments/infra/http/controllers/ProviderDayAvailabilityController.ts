f581f2303eae24b7683e6ebf120ffd845c2a8ac4
Luiz Spinelli <luiz.lucchi@gmail.com>
2020-08-16 12:49:36 -0300

    refactoring availability routes

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -3,24 +3,21 @@ import { container } from 'tsyringe';
 import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
 
 export default class ProviderDayAvailabilityController {
-    public async index(
-        request: Request,
-        response: Response,
-    ): Promise<Response> {
-        const { provider_id } = request.params;
-        const { year, month, day } = request.body;
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { year, month, day } = request.query;
 
-        const listProviderDayAvailability = container.resolve(
-            ListProviderDayAvailabilityService,
-        );
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
 
-        const availability = await listProviderDayAvailability.execute({
-            provider_id,
-            year,
-            month,
-            day,
-        });
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      year: Number(year),
+      month: Number(month),
+      day: Number(day),
+    });
 
-        return response.json(availability);
-    }
+    return response.json(availability);
+  }
 }
--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -3,23 +3,20 @@ import { container } from 'tsyringe';
 import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
 
 export default class ProviderMonthAvailabilityController {
-    public async index(
-        request: Request,
-        response: Response,
-    ): Promise<Response> {
-        const { provider_id } = request.params;
-        const { year, month } = request.body;
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { year, month } = request.query;
 
-        const listProviderMonthAvailability = container.resolve(
-            ListProviderMonthAvailabilityService,
-        );
+    const listProviderMonthAvailability = container.resolve(
+      ListProviderMonthAvailabilityService,
+    );
 
-        const availability = await listProviderMonthAvailability.execute({
-            provider_id,
-            year,
-            month,
-        });
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id,
+      year: Number(year),
+      month: Number(month),
+    });
 
-        return response.json(availability);
-    }
+    return response.json(availability);
+  }
 }
